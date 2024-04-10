b294cabc3d98df897e23a9fcdd65592b368ea187
Kai Pasciak <kai.pasciak@gmail.com>
2024-04-10 12:05:57 -0400

    Created Necessary Files

--- BranchPrediction.py
+++ BranchPrediction.py
@@ -3,6 +3,11 @@
 # Programming Assignment #4
 # Author: Kai Pasciak
 
+# Global variables and arrays to be set later
+MIN_COUNTER = 0
+MAX_COUNTER = 0
+addresses = []
+counters = []
 
 def chooseFile():
     filename = input("Enter the file name to test: ")
