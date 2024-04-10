b60f02479e470c743b842fe85446020daa11f062
Kai Pasciak <kai.pasciak@gmail.com>
2024-04-10 10:15:10 -0400

    Created Necessary Files

---
+++ BranchPrediction.py
@@ -0,0 +1,13 @@
+# Branch Prediction Assignment
+# CS 3220
+# Programming Assignment #4
+# Author: Kai Pasciak
+
+
+def chooseFile():
+    filename = input("Enter the file name to test: ")
+def main():
+    print("Branch Prediction Simulator")
+    chooseFile()
+
+main()
