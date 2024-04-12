729aa8e65a36b0a3baa50ee8cbd9047a02e96fd6
Kai Pasciak <kai.pasciak@gmail.com>
2024-04-12 13:34:39 -0400

    Prompt function

--- BranchPrediction.py
+++ BranchPrediction.py
@@ -6,13 +6,20 @@
 # Global variables and arrays to be set later
 MIN_COUNTER = 0
 MAX_COUNTER = 0
+filename = ""
+counterBits = 0
 addresses = []
 counters = []
 
-def chooseFile():
+def prompt():
     filename = input("Enter the file name to test: ")
+    while (counterBits < 0 and counterBits > 3):
+        counterBits = int(input("Enter the number of bits to use: "))
+    if counterBits == 1:
+        MAX_COUNTER = 2
+    if counterBits ==
 def main():
     print("Branch Prediction Simulator")
-    chooseFile()
+    prompt()
 
 main()
